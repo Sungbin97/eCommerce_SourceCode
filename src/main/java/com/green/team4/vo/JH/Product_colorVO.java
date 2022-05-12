931cfdd85cba56c6b15ece0a65562a223784d6ba
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-05-12 10:16:56 +0900

    option vo 생성

---
+++ src/main/java/com/green/team4/vo/JH/Product_colorVO.java
@@ -0,0 +1,14 @@
+package com.green.team4.vo.JH;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Product_colorVO {
+    private int pc_no ;
+    private String p_code;
+    private String p_color ;
+}
