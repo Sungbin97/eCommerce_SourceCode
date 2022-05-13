0b0008f3535b2e79298bb9be910e44516344cf87
DESKTOP-UL5V77G\swchu <swchung0521@gmail.com>
2022-05-13 17:46:42 +0900

    master 수정

--- src/main/java/com/green/team4/service/sw/MemberInfoService.java
+++ src/main/java/com/green/team4/service/sw/MemberInfoService.java
@@ -3,10 +3,13 @@ package com.green.team4.service.sw;
 import com.green.team4.vo.sw.MemberInfoVO;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MemberInfoService {
 
     MemberInfoVO readOne(int mno);
+
+    public List<MemberInfoVO> readAll();
     int modify(MemberInfoVO memberInfoVO);
     int remove(int mno,String delCategory, String delContent);
 }
