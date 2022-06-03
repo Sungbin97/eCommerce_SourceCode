a0584ac56542b5d6f28db4cf4f4d57921a38b1f6
bs <ptz999@naver.com>
2022-06-03 18:20:29 +0900

    reply 작업2

--- src/main/java/com/green/team4/controller/bs/BoardController.java
+++ src/main/java/com/green/team4/controller/bs/BoardController.java
@@ -5,14 +5,12 @@ import com.green.team4.service.bs.ReplyService;
 import com.green.team4.vo.bs.BoardVO;
 import com.green.team4.vo.bs.Criteria;
 import com.green.team4.vo.bs.PageMaker;
+import com.green.team4.vo.bs.ReplyVO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +22,7 @@ import java.io.IOException;
 public class BoardController {
     @Autowired
     BoardService boardService;
+
     @GetMapping("/list")
     public void list(Model model, Criteria criteria) {
         PageMaker pageMaker = new PageMaker(criteria, boardService.getTotal(criteria));
