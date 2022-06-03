969387b1a73dd4ef7087d374c700c3a186165213
DanKim <everback@naver.com>
2022-06-03 12:14:05 +0900

    2022/06/03 아침 합치기 전 + 작업중코드 주석표시

--- src/main/java/com/green/team4/controller/dan/SignupController.java
+++ src/main/java/com/green/team4/controller/dan/SignupController.java
@@ -1,12 +1,16 @@
 package com.green.team4.controller.dan;
 
 import com.green.team4.service.dan.SignupService;
+import com.green.team4.vo.dan.LoginVO;
 import com.green.team4.vo.dan.SignupVO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,4 +32,19 @@ public class SignupController {
         signupService.insert(signupVO);
         return "redirect:/dan/samplemain";
     }
+
+//    @PostMapping("/signup")
+//    public ResponseEntity<String> SignupPost(@RequestBody SignupVO vo){
+//        System.out.println("로그인 Post Mapping 작동하나 "  +vo);
+//        SignupVO signup = SignupService.insert(vo) ;
+////                = loginService.login(vo);
+//        String success = "";
+////        String fail = "";
+//        if (login != null) success ="성공";
+//        else success ="실패";
+////        fail= "아이디 또는 비밀번호가 맞지 않습니다.";
+//        System.out.println("컨트롤러에서 success 값 확인"+ success);
+//
+//        return new ResponseEntity<>(success, HttpStatus.OK);
+//    }
 }
