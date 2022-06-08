e17684537dd780c29795ab42f5c67b0da6b05636
DESKTOP-UL5V77G\swchu <swchung0521@gmail.com>
2022-06-08 15:08:55 +0900

    20220608 통합 ing4 commit

--- src/main/java/com/green/team4/security/LoginFailHandler.java
+++ src/main/java/com/green/team4/security/LoginFailHandler.java
@@ -36,7 +36,7 @@ public class LoginFailHandler extends SimpleUrlAuthenticationFailureHandler {
             errorMessage = "알 수 없는 이유로 로그인에 실패하였습니다 관리자에게 문의하세요.";
         }
         errorMessage = URLEncoder.encode(errorMessage, "UTF-8");
-        setDefaultFailureUrl("/account/login?error=true&exception="+errorMessage);
+        setDefaultFailureUrl("/login?error=true&exception="+errorMessage);
         super.onAuthenticationFailure(request, response, exception);
     }
 }
