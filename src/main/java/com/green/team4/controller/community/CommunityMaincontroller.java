8f2397b1fd5a91ad430953b4bb0d145ecab3fc0c
bs <ptz999@naver.com>
2022-06-12 11:29:41 +0900

    BS 0610 최종 커밋

--- src/main/java/com/green/team4/controller/community/CommunityMaincontroller.java
+++ src/main/java/com/green/team4/controller/community/CommunityMaincontroller.java
@@ -1,14 +1,22 @@
 package com.green.team4.controller.community;
 
+import com.green.team4.service.community.BoardService;
+import com.green.team4.vo.community.Criteria;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/community/*")
 public class CommunityMaincontroller {
+    @Autowired
+    BoardService boardService;
     @GetMapping("main")
-    public void communityMain(){
-
+    public void communityMain(Model model){
+        Criteria criteria = new Criteria();
+        Long rand = (long)(Math.random()*boardService.getTotal(criteria));
+        model.addAttribute("VO",boardService.getOne(rand));
     }
 }
